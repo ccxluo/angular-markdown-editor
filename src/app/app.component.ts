67801b08137867ff31a5df70771f6b452cdbe7af
cxluo <cxluo@thoughtworks.com>
2020-09-13 17:51:23 +0800

    init markdown editor with editor.md

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {EditorConfig} from './config/EditorConfig';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'markdown-editor';
+
+  /**
+   * editor的配置参数信息
+   */
+  config: EditorConfig;
+
+  /**
+   * markdown的内容
+   */
+  markdown: string;
+
+  constructor() {
+    this.config = new EditorConfig({height: 'calc(100vh - 71px)'});
+    this.markdown = '测试内容';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditorMdDirective } from './editor-md.directive';
+import { EditorMarkdownComponent } from './editor-markdown/editor-markdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorMdDirective,
+    EditorMarkdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
